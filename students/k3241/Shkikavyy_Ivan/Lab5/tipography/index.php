b4b40a656547249cc5d592cdded649eee78154a9
bigcoachpotato <48162523+bigcoachpotato@users.noreply.github.com>
2020-06-04 11:45:28 +0300

    Revert "Revert "Лабораторная работа №5""
    This reverts commit a54aa3310cc532ba106725c47cefd9c2b41eeef9.

---
+++ students/k3241/Shkikavyy_Ivan/Lab5/tipography/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <title>Редактор БД "Газетная сеть"</title>
+</head>
+<body>
+<a href="newspaper.php">"Газеты"</a> <br/>
+<a href="edition.php">"Тираж"</a><br/>
+<a href="print.php">"Печать"</a><br/>
+<a href="tipography.php">"Типографии"</a><br/>
+<a href="distribution.php">"Распределение"</a><br/>
+<a href="postoffice.php">"Почты"</a><br/>
+</body>
+
+<b>Запросы лабы №4:</b></br>
+<?php
+$dbuser = 'postgres';
+$dbpassword = '1234';
+$host = 'localhost';
+$dbname = 'lab3';
+
+$pdo = new PDO("pgsql:host=$host;dbname=$dbname", $dbuser, $dbpassword);
+$sql1= 'SELECT * FROM public."Postoffice" WHERE "Post_adress" LIKE \'%д.34%\' OR "Branch_number" > 10';
+$sql2= 'SELECT * FROM public."Newspaper" INNER JOIN public."Edition" on "Edition"."ID_Newspaper"="Newspaper"."ID_Newspaper" ORDER BY "Publication_number"'; 
+$sql3= 'SELECT * FROM public."Newspaper" WHERE POSITION(btrim(LOWER(\' Сергей \')) in LOWER ("Reductor"))>0 OR POSITION(btrim(LOWER(\'С\')) in LOWER("Naming"))>0';
+$sql4= 'SELECT COUNT(DISTINCT "Printed_quantity") AS "Quantity_of_1_ID_Edition" FROM public."Print" WHERE "Printed_quantity" IN (SELECT "Printed_quantity" FROM public."Print" WHERE "ID_Edition" = 1)';
+$sql5= 'SELECT "Newspaper"."Naming", "Newspaper"."Index", "Newspaper"."Reductor", MIN ("Print"."Printed_quantity") FROM public."Edition" INNER JOIN public."Newspaper" ON "Edition"."ID_Newspaper"="Newspaper"."ID_Newspaper" INNER JOIN public."Print" ON "Edition"."ID_Edition"="Print"."ID_Edition"GROUP BY "Newspaper"."Naming", "Newspaper"."Index", "Newspaper"."Reductor" HAVING MIN ("Print"."Printed_quantity")<11';
+$reqs = array($sql1, $sql2, $sql3, $sql4, $sql5);
+foreach ($reqs as $value) {
+    foreach ($pdo->query($value) as $row) {
+        print_r($row);
+    }
+    echo "</br>";
+    echo "</br>";
+}
+?>
+</html>
---
+++ students/k3241/Shkikavyy_Ivan/Lab5/tipography/newspaper.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Газеты</title>
+</head>
+<body>
+<?php
+$data = null;
+$status = "";
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $dbuser = 'postgres';
+    $dbpassword = '1234';
+    $host = 'localhost';
+    $dbname = 'lab3';
+    $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $dbuser , $dbpassword );
+    if(isset($_POST["find"])) {
+        $sql = 'SELECT * from public."Newspaper" where "ID_Newspaper" = :ID_Newspaper';
+        $sth = $pdo->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+        $sth->execute(array(':ID_Newspaper' => $_POST["ID_Newspaper"]));
+        $data = $sth->fetchAll();
+        if(count($data) > 0){
+            $status = "Результат:";
+        }else{
+            $status = "Информации нет.";
+        }
+    }
+	elseif(isset($_POST["edit"])) {
+		if($_POST["ID_Newspaper"] != ""){
+			$sql = 'SELECT * from public."Newspaper" where "ID_Newspaper" = :ID_Newspaper';
+			$sth = $pdo->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+			$sth->execute(array(':ID_Newspaper' => intval($_POST["ID_Newspaper"])));
+			$data = $sth->fetchAll();
+		}
+		elseif($_POST["ID_Newspaper"] != "" && count($data) > 0){
+			$sql = 'UPDATE public."Newspaper" SET "Naming"= :Naming, "Index"= :Index, "Reductor" = :Reductor where "ID_Newspaper" = :ID_Newspaper';
+			$sth = $pdo->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+			$sth->execute(array(':ID_Newspaper' => $_POST["ID_Newspaper"],':Naming' => $_POST["Naming"],':Index' => $_POST["Index"],':Reductor' => $_POST["Reductor"]));
+			$data = $sth->fetchAll();
+			$status = "Изменено";
+			$data = null;
+		}
+    }
+	elseif(isset($_POST["insert"])){
+		$sql = 'INSERT INTO public."Newspaper"("Naming", "Index", "Reductor") VALUES (:Naming, :Index, :Reductor)';
+		$sth = $pdo->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+		$sth->execute(array(':Naming' => $_POST["Naming"],':Index' => $_POST["Index"],':Reductor' => $_POST["Reductor"]));
+		$data = $sth->fetchAll();
+		 $status = "Добавлено";
+		 $data = null;
+	}
+	elseif(isset($_POST["delete"])) {
+        $sql = 'delete from public."Newspaper" where "ID_Newspaper" = :ID_Newspaper';
+        $sth = $pdo->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+        $sth->execute(array(':ID_Newspaper' => $_POST["ID_Newspaper"]));
+        $data = $sth->fetchAll();
+        $status = "Удалено.";
+        $data = null;  
+	}
+}
+?>
+<form action="" method="post">
+	Введите ID </br>
+    <input name="ID_Newspaper"  value="<?php echo '' ?>"> </br>
+    <button type="submit" name="find">Поиск</button>
+    <button type="submit" name="delete">Удаление</button>
+</form>
+<?php echo $status ?>
+</br>
+<form action="" method="post">
+	 Идентификатор газеты</br>
+    <input name="ID_Newspaper" size="30" value="<?php if($_SERVER['REQUEST_METHOD'] == 'POST' && $data) echo $data[0]['ID_Newspaper']?>">
+	 <br>Бренд газеты</br>
+    <input name="Naming" size="30" value="<?php if($_SERVER['REQUEST_METHOD'] == 'POST' && $data) echo $data[0]['Naming']?>">
+	 <br>Индекс</br>
+    <input name="Index" size="30" value="<?php if($_SERVER['REQUEST_METHOD'] == 'POST' && $data) echo $data[0]['Index']?>">
+	 <br>Редактор</br>
+    <input name="Reductor" size="30" value="<?php if($_SERVER['REQUEST_METHOD'] == 'POST' && $data) echo $data[0]['Reductor']?>"></br>
+    <button type="submit" name="insert">Добавление</button>
+	<button type="submit" name="edit">Редактирование</button>
+	<br><a href="index.php">"Назад"</a><br/>
+</form>
+</body>
+</html>
+
