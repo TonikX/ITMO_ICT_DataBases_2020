435c523e592f6254e88d5dd83bb7ada57f1a793d
Vladimir <vladimir27goncharov@icloud.com>
2020-06-12 15:39:44 +0300

    5 queries added

--- students/k3240/Goncharov_Vladimir/Lab05/Lab/index.php
+++ students/k3240/Goncharov_Vladimir/Lab05/Lab/index.php
@@ -2,3 +2,4 @@
 <p><a href="enrollee.php">К абитуриентам</a></p>
 <p><a href="passport.php">К паспортам</a></p>
 <p><a href="school.php">К школам</a></p>
+<p><a href="queries.php">К запросам</a></p>
